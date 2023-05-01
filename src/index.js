4bb8687654be4c32ddc85c9025242e7e03485fbb
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2023-05-01 10:01:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-tweets-goit">
+    <BrowserRouter basename="/test-tweets-goit/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
